56fb57cdecac4c7f00ad343a7e78b67fa25d749d
cutestchicken <152752449+cutestchicken@users.noreply.github.com>
2023-12-02 17:35:29 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,348 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="img/1.2.png" type="image/x-icon">
+    <title>Bienvenidos - Inicio Cutest Chicken Store</title>
+    <link rel="stylesheet" href="css2,0.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&family=Mulish&family=Noto+Color+Emoji&family=Playfair+Display&family=Raleway&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js" > </script>
+    <script>
+            $(document).ready(function(){
+                    $("a[title|='Hosted on free web hosting 000webhost.com. Host your own website for FREE.']").css("display", "none");
+                    $("img[alt|='www.000webhost.com']").css("display", "none");
+                });
+    </script>
+</head>
+<body>
+    <header>
+        <center>
+            <img src="img/descarga-removebg-preview.png" alt="Cutest Chicken" class="icon">
+            <!--<img src="img/" alt="">
+            <h1>Cutest Chicken</h1>--></center>
+            <style>.icon{height: 200px;}</style>
+        <div class="menu">
+            <nav>
+                <div class="menu11">
+                    <ul class="menu1">
+                        <li><a href="#">Populares</a></li>
+                        <li><a href="#">Ofertas</a></li>
+                        <li><a href="productos/productos.php">Productos</a></li>
+                        <li><a href="#nosotros">Nosotros</a></li>
+                    </ul>
+                </div>
+                <div class="menu21">
+                    <ul class="menu2">
+                        <li>
+                            <div class="buscador1">
+                                <input type="search" name="buscar" id="buscar">
+                                <input type="search" placeholder="Buscar . . ." id="inputBusqueda"/>
+                            </div>
+                        </li>
+                        <li>
+                            <input type="checkbox" name="" id="buscador">
+                            <label for="buscador"><img src="img/buscar.png" alt="buscar" class="busqicon"></label>
+                        </li>
+                        <li>
+                        <a href="viewCart.php" class="cart-link" title="View Cart"><img src="img/carrito.png" alt="carrito de compras" class="menuicon"></a>
+                        </li>                       
+                        <li>
+                            <label for="btnmenu"><img src="img/menu.png" alt="menu" class="menuicon"></label>
+                        </li>
+                    </ul>
+                </div>
+                
+                <input type="checkbox" name="" id="btnmenu">
+                <nav class="menuPDCTS">
+                    <ul class="listA">
+                        <li>
+                            <a href="productos/amigurumis/amigurumis.html">Amigurumis</a>
+                            <ul class="listB">
+                                <li>
+                                    <a href="productos/amigurumis/animales.php">Animales</a>
+                                </li>
+                                <li>
+                                    <a href="productos/amigurumis/personajes.html">Personajes</a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <a href="productos/amigurumis/amigurumis.html">Plantas</a>
+                            <ul class="listB">
+                                <li>
+                                    <a href="productos/amigurumis/animales.html">Suculentas</a>
+                                </li>
+                                <li>
+                                    <a href="productos/amigurumis/personajes.html">Flores</a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <a href="productos/accesorios/accesorios.html">Accesorios</a>
+                            <ul class="listB">
+                                <li>
+                                    <a href="productos/accesorios/llaveros.html">Llaveros</a>
+                                </li>
+                                <li>
+                                    <a href="productos/accesorios/chockers.html">Chockers</a>
+                                </li>
+                                <li>
+                                    <a href="productos/accesorios/pulseras.html">Pulseras</a>
+                                </li>
+                                <li>
+                                    <a href="productos/accesorios/audifonos.html">Audífonos</a>
+                                </li>
+                                <li>
+                                    <a href="productos/accesorios/bolsos.html">Bolsos</a>
+                                </li>
+                            </ul>
+                        </li>
+                          
+                        <li>
+                            <a href="productos/pies/pies.html">Pies</a>
+                            <ul class="listB"   >
+                                <li>
+                                    <a href="productos/pies/calcetines.html">Calcetines</a>
+                                </li>
+                                <li>
+                                    <a href="productos/pies/calentadores.html">Calentadores</a>
+                                </li>
+                                <li>
+                                    <a href="productos/pies/medias.html">Medias</a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <a href="productos/decoraciones/decoraciones.html">Decoraciones</a>
+                            <ul class="listB"   >
+                                <li>
+                                    <a href="productos/decoraciones/guirnaldas.html">Guirnaldas</a>
+                                </li>
+                                <li>
+                                    <a href="productos/decoraciones/cojines.html">Cojines</a>
+                                </li>
+                                <li>
+                                    <a href="productos/decoraciones/gadgets.html">Gadgets</a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <a href="productos/tops/tops.html">Tops</a>
+                            <ul class="listB">
+                                <li>
+                                    <a href="productos/tops/chalecos.html">Chalecos</a>
+                                </li>
+                                <li>
+                                    <a href="productos/tops/redes.html">Redes</a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <a href="productos/sweaters/sweaters.html">Sweaters</a>
+                            <ul class="listB">
+                                <li>
+                                    <a href="productos/sweaters/cardigan.html">Cardigan</a>
+                                </li>
+                                <li>
+                                    <a href="productos/sweaters/aborregados.html">Aborregados</a>
+                                </li>
+                                <li>
+                                    <a href="productos/sweaters/oversize.html">Oversize</a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <a href="productos/herramientas/herramientas.html">Herramientas</a>
+                            <ul class="listB"   >
+                                <li>
+                                    <a href="productos/herramientas/agujasyganchillos.html">Ganchillos y agujas</a>
+                                </li>
+                                <li>
+                                    <a href="productos/herramientas/complementos.html">Complementos</a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <a href="productos/materiales/materiales.html">Materiales</a>
+                            <ul class="listB"   >
+                                <li>
+                                    <a href="productos/materiales/estambres.html">Estambres</a>
+                                </li>
+                                <li>
+                                    <a href="productos/materiales/hilos.html">Hilos</a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <a href="productos/kits/kits.html">Kits</a>
+                            <ul class="listB">
+                                <li>
+                                    <a href="productos/kits/principiantes.html">Principiantes</a>
+                                </li>
+                                <li>
+                                    <a href="productos/kits/crochet.html">Crochet</a>
+                                </li>
+                                <li>
+                                    <a href="productos/kits/amigurumiskits.html">Amigurumis</a>
+                                </li>
+                                <li>
+                                    <a href="productos/kits/punto.html">Punto</a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <a href="productos/otros/otros.html">Otros</a>
+                            <ul class="listB">
+                                <li>
+                                    <a href="productos/otros/personalizados.html">Personalizados</a>
+                                </li>
+                                <li>
+                                    <a href="productos/otros/mantas.html">Mantas</a>
+                                </li>
+                            </ul>
+                        </li>
+                            
+                    </ul>
+                </nav>
+            </nav>
+        </div>
+    </header>
+    <section>
+        <!-- Busqueda -->
+        <div class="search" id="search">
+          <table class="search-tabla" id="searchTabla">
+            <thead><tr><td></td></tr></thead>
+            <tbody id="tabla"></tbody>
+          </table>
+        </div>
+        <script src="js/buscador.js"></script>
+        <div class="intro">
+            <h3>¡Bienvenidx!</h3>
+            <p>Siéntete libre de explorar cuanto quieras, 
+                esperamos que encuentres lo que estes buscando 
+                y/o algo que sea de tu agrado.
+            </p>
+        </div>
+        <div class="intro">
+            <h3>Secciones habilitadas</h3>
+            <p>Esta página aún se encuentra en desarrollo, por lo que algunas
+                funciones podrían no funcionar adecuadamente o tener un formato
+                disfuncional, seguimos trabajando para poder brindar una mejor experiencia.
+                
+            </p>
+        </div>
+    </section>
+    <footer>
+        <div class="registro">
+            <form action="" method="post">
+                <h2>¡Suscribete para recibir promociones y estar siempre al tanto de las novedades!</h2>
+                <div class="form">
+                    <h3>Ingresa los siguientes datos:</h3>
+                    <strong><label for="">Nombre: </label></strong>
+                    <input type="text" name="nombre" class="txtbx" required><br><br>
+                    <strong><label for="">Correo electrónico: </label></strong>
+                    <input type="email" name="newslwtter" id="newsletter" class="txtbx" required><br><br>
+                    <strong><label for="">Número de teléfono: </label></strong>
+                    <input type="tel" name="cel" class="txtbx" required>
+                    <label for="anuncio"><input type="submit" value="Registrarme" class="btn"></label>
+                    <input type="checkbox" id="anuncio" style="display: none;"><br><br>
+                </div>
+            </form>
+        </div>
+        <?php
+            include 'productos/amigurumis/dbConfig.php';
+            if(!$db)
+            {
+                die("Conexión Fallida" . mysqli_connect_error());
+            }
+            else{
+                $name = $_POST['nombre'];
+                $email = $_POST['newslwtter'];
+                $phone = $_POST['cel'];
+                $action = "INSERT INTO `customers`(`id`, `name`, `email`, `phone`, `address`, `created`, `modified`, `status`) 
+                VALUES ('NULL','$name','$email','$phone','qq','NULL','NULL','1');";
+                if (mysqli_query($db, $action))
+                {
+                    echo "Gracias por registrarte!!";
+                }
+                else
+                {
+                    echo "Error: <br>" . mysqli_error($db);
+                }
+                mysqli_close($db);
+            }
+        ?>
+        <hr>
+        <table>
+            <tr id="nosotros">
+                <th><h3 class="nosotros">Nosotros</h3></th>
+                <th id="contcfoot">Contáctanos</th>
+                <th>Atención al cliente</th>
+                <th>Términos y condiciones</th>
+            </tr>
+            <tr>
+                <td class="nosotros">Somos una tienda mexicana que busca extender los 
+                        alcances del mundo del tejido y el handmade, tanto 
+                        para creadores como para fanáticos o amigos que no 
+                        practiquen este arte y quieran acercarse un poquito 
+                        más a él, esto, proporcionándoles las herramientas 
+                        necesarias para realizar sus proyectos y adquirir 
+                        artículos de calidad y diseño.
+                </td>
+                <td id="datosfoot">
+                    <ul class="foot">
+                        <li><a href="https://maps.app.goo.gl/XKJW5MbmZyqDT5oc6">
+                            Carr. Transpeninsular km 115, Ex-Ejido Chapultepec, 
+                            <br>22875 Ensenada, B.C.</a></li>
+                        <li><br></li>
+                        <li><b>Horario de atención</b></li>
+                        <li>Lunes a viernes 9:00 - 17:00</li>
+                        <li><br></li>
+                        <li>WhatsApp: <a href="tel:4434649831">443-464-9831</a></li>
+                        <li>Correo: <a href="cutestchickenstore@gmai.com">cutestchickenstore@gmai.com</a></li>
+                        <li>Redes sociales:</li>
+                            <li><a href="https://www.instagram.com/cutestchickenstore"><img src="img/insta.png" alt="Instagram"></a>
+                                <a href="https://www.facebook.com/cutestchickenstore"><img src="img/fb.png" alt="Facebook"></a>
+                                <a href="https://www.pinterest.com.mx/cutestchickenstore"><img src="img/pinterest.png" alt="pinterest"></a>
+                            </li>
+                    </ul>
+                </td>
+                <td>
+                    <ul class="foot">
+                        <li><a href="#">Mi cuenta</a></li>
+                        <li><a href="terminos-y-condiciones/FAQ.html">Preguntas frecuentes</a></li>
+                        <li><a href="#">Mapa del sitio</a></li>
+                    </ul>
+                </td>
+                <td>
+                    <ul class="foot">
+                        <li><a href="terminos-y-condiciones/aviso-de-copyright.html" target="_blank">Aviso de Copyright</a></li>
+                        <li><a href="terminos-y-condiciones/aviso-de-privacidad.html" target="_blank">Aviso de privacidad</a></li>
+                        <li><a href="terminos-y-condiciones/condiciones-de-compra.html" target="_blank">Condiciones de compra</a></li>
+                        <li><a href="terminos-y-condiciones/devoluciones.html" target="_blank">Devoluciones</a></li>
+                        <li><a href="terminos-y-condiciones/condiciones-de-compra.html">Envíos</a></li>
+                        <li><a href="terminos-y-condiciones/aviso-de-privacidad.html">Términos de servicio</a></li>
+                    </ul>
+                </td>
+            </tr>
+        </table>
+        <p>&copy; 2023 Cutest Chicken Todos los derechos reservados. </p>
+        <p>**Cutest Chicken se trata de un proyecto escolar 
+            que busca poner en práctica las metodologías de 
+            desarrollo web y bases de datos en el e-commerce.</p>
+    </footer>
+</body>
+</html>
